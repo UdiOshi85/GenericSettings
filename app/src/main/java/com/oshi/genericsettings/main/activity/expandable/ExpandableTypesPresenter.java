f0d5da84e6d52a5ea9cdee6aa23350c879e90793
udi oshi <udioshi@gmail.com>
2017-09-26 10:20:28 +0300

    + Almost ready for next release

--- app/src/main/java/com/oshi/genericsettings/main/activity/expandable/ExpandableTypesPresenter.java
+++ app/src/main/java/com/oshi/genericsettings/main/activity/expandable/ExpandableTypesPresenter.java
@@ -2,20 +2,20 @@ package com.oshi.genericsettings.main.activity.expandable;
 
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
+import android.util.Log;
 import android.view.View;
 
 import com.oshi.genericsettings.R;
 import com.oshi.libgenericsettings.data.BaseViewTypeData;
-import com.oshi.libgenericsettings.data.CheckableSubItem;
 import com.oshi.libgenericsettings.data.ExpandableTitleCheckableItemsData;
 import com.oshi.libgenericsettings.data.ExpandableTitleSimpleItemsData;
 import com.oshi.libgenericsettings.data.ExpandableTitleSubtitleCheckableItemsData;
 import com.oshi.libgenericsettings.data.ExpandableTitleSubtitleSimpleItemsData;
 import com.oshi.libgenericsettings.data.HeaderData;
+import com.oshi.genericsettings.main.activity.utils.ArrayUtils;
 import com.oshi.libgenericsettings.presenter.BaseSettingsPresenter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -50,52 +50,55 @@ public class ExpandableTypesPresenter extends BaseSettingsPresenter {
         items.add(POSITION_HEADER, headerData);
 
 
-        List<String> simpleItemsList = Arrays.asList("First", "Second", "Third");
-        List<CheckableSubItem> checkableItemsList = Arrays.asList(
-                new CheckableSubItem("First"),
-                new CheckableSubItem("Second"),
-                new CheckableSubItem("Third"));
-
-        ExpandableTitleSimpleItemsData expandableTitleSimpleItemsData = new ExpandableTitleSimpleItemsData("Expandable title with simple items", simpleItemsList);
+        ExpandableTitleSimpleItemsData expandableTitleSimpleItemsData = new ExpandableTitleSimpleItemsData("Expandable title with simple items", ArrayUtils.getNewSimpleItemsList());
         items.add(POSITION_EXPANDABLE_TITLE_SIMPLE_ITEMS, expandableTitleSimpleItemsData);
 
-        ExpandableTitleSubtitleSimpleItemsData expandableTitleSubtitleSimpleItemsData = new ExpandableTitleSubtitleSimpleItemsData("Expandable title & subtitle", "Simple items", simpleItemsList);
+        ExpandableTitleSubtitleSimpleItemsData expandableTitleSubtitleSimpleItemsData = new ExpandableTitleSubtitleSimpleItemsData("Expandable title & subtitle", "Simple items",  ArrayUtils.getNewSimpleItemsList());
         items.add(POSITION_EXPANDABLE_TITLE_SUBTITLE_SIMPLE_ITEMS, expandableTitleSubtitleSimpleItemsData);
 
-        ExpandableTitleCheckableItemsData expandableTitleCheckableItemsData = new ExpandableTitleCheckableItemsData("Expandable title with checkable items", checkableItemsList);
+        ExpandableTitleCheckableItemsData expandableTitleCheckableItemsData = new ExpandableTitleCheckableItemsData("Expandable title with checkable items", ArrayUtils.getNewCheckableItemsList());
         items.add(POSITION_EXPANDABLE_TITLE_CHECKABLE_ITEMS, expandableTitleCheckableItemsData);
 
-        ExpandableTitleSubtitleCheckableItemsData expandableTitleSubtitleCheckableItemsData = new ExpandableTitleSubtitleCheckableItemsData("Expandable title with subtitle", "Checkable items", checkableItemsList);
+        ExpandableTitleSubtitleCheckableItemsData expandableTitleSubtitleCheckableItemsData = new ExpandableTitleSubtitleCheckableItemsData("Expandable title with subtitle", "Checkable items", ArrayUtils.getNewCheckableItemsList());
         items.add(POSITION_EXPANDABLE_TITLE_SUBTITLE_CHECKABLE_ITEMS, expandableTitleSubtitleCheckableItemsData);
 
         HeaderData coloredHeaderData = new HeaderData("COLORED EXPANDABLE TYPES", R.color.blue);
         items.add(POSITION_COLORED_HEADER, coloredHeaderData);
 
-        ExpandableTitleSimpleItemsData coloredExpandableTitleSimpleItemsData = new ExpandableTitleSimpleItemsData("Colored title with simple colored items", simpleItemsList);
+        ExpandableTitleSimpleItemsData coloredExpandableTitleSimpleItemsData = new ExpandableTitleSimpleItemsData("Colored title with simple colored items",  ArrayUtils.getNewSimpleItemsList());
         coloredExpandableTitleSimpleItemsData.setTitleColor(R.color.blue);
         coloredExpandableTitleSimpleItemsData.setIconColor(R.color.blue);
         coloredExpandableTitleSimpleItemsData.setItemsColor(R.color.blue);
         items.add(POSITION_COLORED_EXPANDABLE_TITLE_SIMPLE_ITEMS, coloredExpandableTitleSimpleItemsData);
 
-        ExpandableTitleSubtitleSimpleItemsData coloredExpandableTitleSubtitleSimpleItemsData = new ExpandableTitleSubtitleSimpleItemsData("Colored title with subtitle", "Simple Items", simpleItemsList);
+        ExpandableTitleSubtitleSimpleItemsData coloredExpandableTitleSubtitleSimpleItemsData = new ExpandableTitleSubtitleSimpleItemsData("Colored title with subtitle", "Simple Items",  ArrayUtils.getNewSimpleItemsList());
         coloredExpandableTitleSubtitleSimpleItemsData.setTitleColor(R.color.blue);
         coloredExpandableTitleSubtitleSimpleItemsData.setSubtitleColor(R.color.red_text);
         coloredExpandableTitleSubtitleSimpleItemsData.setIconColor(R.color.light_green);
         coloredExpandableTitleSubtitleSimpleItemsData.setItemsColor(R.color.orange);
         items.add(POSITION_COLORED_EXPANDABLE_TITLE_SUBTITLE_SIMPLE_ITEMS, coloredExpandableTitleSubtitleSimpleItemsData);
 
-        ExpandableTitleCheckableItemsData coloredExpandableTitleCheckableItemsData = new ExpandableTitleCheckableItemsData("Colored title with checkable colored", checkableItemsList);
+        ExpandableTitleCheckableItemsData coloredExpandableTitleCheckableItemsData = new ExpandableTitleCheckableItemsData("Colored title with checkable colored", ArrayUtils.getNewCheckableItemsList());
         coloredExpandableTitleCheckableItemsData.setTitleColor(R.color.purple);
         coloredExpandableTitleCheckableItemsData.setItemsColor(R.color.purple);
         coloredExpandableTitleCheckableItemsData.setCheckboxColor(R.color.purple);
         coloredExpandableTitleCheckableItemsData.setIconColor(R.color.purple);
         items.add(POSITION_COLORED_EXPANDABLE_TITLE_CHECKABLE_ITEMS, coloredExpandableTitleCheckableItemsData);
 
+        ExpandableTitleSubtitleCheckableItemsData coloredExpandableTitleSubtitleCheckableItemsData = new ExpandableTitleSubtitleCheckableItemsData("Colored title & subtitle", "Checkable Items", ArrayUtils.getNewCheckableItemsList());
+        coloredExpandableTitleSubtitleCheckableItemsData.setTitleColor(R.color.colorPrimary);
+        coloredExpandableTitleSubtitleCheckableItemsData.setSubtitleColor(R.color.colorPrimary);
+        coloredExpandableTitleSubtitleCheckableItemsData.setCheckboxColor(R.color.orange);
+        coloredExpandableTitleSubtitleCheckableItemsData.setDividerColor(R.color.red);
+        coloredExpandableTitleSubtitleCheckableItemsData.setShouldShowDivider(true);
+        items.add(POSITION_COLORED_EXPANDABLE_TITLE_SUBTITLE_CHECKABLE_ITEMS, coloredExpandableTitleSubtitleCheckableItemsData);
+
         return items;
     }
 
     @Override
     public void onExpandableCheckableItemClicked(View view, ExpandableTitleCheckableItemsData data, int parentPosition, int subItemPosition) {
+        Log.d(ExpandableTypesPresenter.class.getSimpleName(), "onExpandableCheckableItemClicked: Position: " + parentPosition + ", Sub item: " + subItemPosition);
         boolean oldVal = data.getItems().get(subItemPosition).isChecked();
         boolean newVal = !oldVal;
         data.getItems().get(subItemPosition).setChecked(newVal);
@@ -105,5 +108,6 @@ public class ExpandableTypesPresenter extends BaseSettingsPresenter {
     @Override
     public void onExpandableSimpleItemClicked(View view, ExpandableTitleSimpleItemsData data, int parentPosition, int subItemPosition) {
         super.onExpandableSimpleItemClicked(view, data, parentPosition, subItemPosition);
+        Log.d(ExpandableTypesPresenter.class.getSimpleName(), "onExpandableSimpleItemClicked: Position: " + parentPosition + ", Sub item: " + subItemPosition);
     }
 }

545ab7428624df2ab8e4c1cdd108e83f2c30e7ff
udi oshi <udioshi@gmail.com>
2018-01-31 12:55:59 +0200

    + Not ready for analytics yet

---
+++ app/src/main/java/com/oshi/genericsettings/GenericSettingsApp.kt
@@ -0,0 +1,13 @@
+package com.oshi.genericsettings
+
+import android.app.Application
+
+class GenericSettingsApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+}
